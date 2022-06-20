6661759a4a2a453656fe8cad84bcf5f596ac7852
bax2004rj <bax2004rj@gmail.com>
2022-06-19 22:51:27 -0400

    Started basic implementation

---
+++ browserTab.py
@@ -0,0 +1,26 @@
+import tkinter
+from tkinter import ttk
+from tkinterweb import HtmlFrame
+
+class newTab():
+    def __init__(self,tab,stringVar,startpage):
+        self.addressObject = tkinter.Frame(tab)
+        self.addressObject.pack(side = "top",fill = "x")
+        
+        self.currentAddress = tkinter.StringVar(tab,value = startpage)
+
+        self.addressBar = ttk.Entry(self.addressObject,textvariable = self.currentAddress)
+        self.addressBar.bind("<Return>",self.goToPage)
+        self.addressBar.pack(fill = "x")
+
+        self.gobutton = ttk.Button(self.addressBar,style="Accent.TButton",text = "Go")
+        self.gobutton.pack(side="right")
+
+        self.browser = HtmlFrame(tab)
+        self.browser.pack(fill="both", expand=True)
+
+        self.browser.load_website(startpage)
+
+    def goToPage(self,event = None):
+        page = self.currentAddress.get()
+        self.browser.load_website(page)
---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter
+from tkinter import ttk
+import sv_ttk
+# Other imports
+import browserTab
+
+class main():
+    def __init__(self):
+        self.app = tkinter.Tk()
+        self.app.winfo_toplevel().title("Flamingearth Browser v1.0a")
+        self.app.geometry("1366x720")
+
+        sv_ttk.set_theme("dark") # Enable darkmode
+
+        self.addcloseframe = ttk.Frame(self.app)
+        self.addcloseframe.pack(side="top", fill = "x")
+
+        self.newTab = ttk.Button(self.addcloseframe,text="+",command= self.tabAdd) # Tab adder
+        self.newTab.pack(side="right")
+
+        self.closeTab = ttk.Button(self.addcloseframe,text="X") # Tab closer
+        self.closeTab.pack(side="right")
+
+        self.tabs = ttk.Notebook(self.app) # Create tabs
+        self.tabs.pack(fill="both",expand=1)
+
+        self.tabObjects = []
+        self.tabFrames = []
+        self.tabVars = []
+    
+    def tabAdd(self,page = "http://www.google.com/"): # Create new tab in browserTab module
+        newFrame = ttk.Frame(self.tabs)
+        newTab = self.tabs.add(newFrame,text="New tab")
+        self.tabVars.append(tkinter.StringVar(self.app,"New tab"))
+        self.tabFrames.append(newFrame)
+        self.tabObjects.append(newTab) # Formerly textVariable = self.tabVars[-1]
+        print ("New tab generated")
+        browserTab.newTab(self.tabObjects[-1],self.tabVars[-1],page)
+
+browser = main()
+browser.tabAdd()
+browser.app.mainloop()

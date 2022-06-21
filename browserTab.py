a0840ce2a8cce6d28e85342984fda3ed68e6d05d
bax2004rj <bax2004rj@gmail.com>
2022-06-20 23:05:38 -0400

    Added menu system, with zoom function

--- browserTab.py
+++ browserTab.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinterweb import HtmlFrame
 
 crashHandling = True
+homePage = "https://www.google.com"
 
 class newTab():
     def __init__(self,tab,stringVar,startpage):
@@ -11,13 +12,53 @@ class newTab():
         
         self.currentAddress = tkinter.StringVar(tab,value = startpage)
 
-        self.addressBar = ttk.Entry(self.addressObject,textvariable = self.currentAddress)
+        self.addressBar = ttk.Combobox(self.addressObject,textvariable = self.currentAddress)
         self.addressBar.bind("<Return>",self.goToPage)
         self.addressBar.pack(fill = "x")
 
-        self.gobutton = ttk.Button(self.addressBar,style="Accent.TButton",text = "Go")
+        self.historyMenu = tkinter.Menu(self.addressBar)
+        self.historyMenu.add_command(label="Clear history")
+
+        self.downloadMenu = tkinter.Menu(self.addressBar)
+        self.downloadMenu.add_command(label="Clear downloads")
+
+        self.zoom = 1
+
+        self.zoomMenu = tkinter.Menu(self.addressBar)
+        self.zoomMenu.add_command(label="Zoom level: 100%",state="disabled")
+        self.zoomMenu.add_command(label="+25%",command = self.zoomIn)
+        self.zoomMenu.add_command(label="Reset",command = self.zoomReset)
+        self.zoomMenu.add_command(label="-25%",command = self.zoomOut)
+
+        self.bookmarksMenu = tkinter.Menu(self.addressBar)
+
+        self.hamburgerMenu = tkinter.Menu(self.addressBar)
+        self.hamburgerMenu.add_command(label="New tab")
+        self.hamburgerMenu.add_command(label="New window")
+        self.hamburgerMenu.add_separator()
+        self.hamburgerMenu.add_cascade(label="History",menu=self.historyMenu)
+        self.hamburgerMenu.add_cascade(label="Downloads",menu=self.downloadMenu)
+        self.hamburgerMenu.add_cascade(label="Bookmarks",menu=self.bookmarksMenu)
+        self.hamburgerMenu.add_separator()
+        self.hamburgerMenu.add_cascade(label="Zoom",menu=self.zoomMenu)
+
+        self.menuButton = ttk.Menubutton(self.addressBar,text = "≡",menu = self.hamburgerMenu)
+        self.menuButton.pack(side = "right")
+
+        self.menuButton = ttk.Menubutton(self.addressBar,text = "↓",menu = self.downloadMenu)
+        self.menuButton.pack(side = "right")
+
+        self.homeButton = ttk.Button(self.addressBar,text = "⌂️",command = self.goHome)
+        self.homeButton.pack(side = "right")
+
+        self.refreshButton = ttk.Button(self.addressBar,text = "↺",command= self.refresh)
+        self.refreshButton.pack(side="right")
+
+        self.gobutton = ttk.Button(self.addressBar,style="Accent.TButton",text = "Go",command=newTab.goToPage)
         self.gobutton.pack(side="right")
 
+        # Setup browser object
+        
         self.browser = HtmlFrame(tab)
         self.browser.pack(fill="both", expand=True)
 
@@ -25,5 +66,23 @@ class newTab():
         self.browser.load_website(startpage)
 
     def goToPage(self,event = None):
-        page = self.currentAddress.get()
-        self.browser.load_website(page)
+        self.page = self.currentAddress.get()
+        self.browser.load_website(self.page)
+    
+    def refresh(self):
+        self.browser.load_website(self.page)
+    
+    def goHome(self):
+        self.browser.load_website(homePage)
+
+    def zoomIn(self):
+        self.zoom += .25
+        self.browser.set_zoom(self.zoom)
+    
+    def zoomOut(self):
+        self.zoom -= .25
+        self.browser.set_zoom(self.zoom)
+    
+    def zoomReset(self):
+        self.zoom = 1
+        self.browser.set_zoom(self.zoom)

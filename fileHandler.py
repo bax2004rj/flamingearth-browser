7ceaba29b5c5ab8fc70936605a64e71bfaacaac2
bax2004rj <bax2004rj@gmail.com>
2022-09-28 02:18:41 -0400

    Started newtab shortcut system

---
+++ fileHandler.py
@@ -0,0 +1,10 @@
+import os
+
+## Get icon file locations
+cwd = os.getcwd()
+iconFolderPNG = os.path.join(cwd,"icons","png")
+iconFolderSVG = os.path.join(cwd,"icons","png")
+
+# Files
+noIcon = os.path.join(iconFolderPNG,"noIconPage.png")
+noShortcut = os.path.join(iconFolderPNG,"noShortcut.png")
--- newTab.py
+++ newTab.py
@@ -1,35 +1,44 @@
 import tkinter
 from tkinter import ttk
 from tkinterweb import HtmlFrame
+from PIL import ImageTk, Image
+import fileHandler
 
 class newTab():
     def __init__(self,tab,stringVar,startpage):
+        ## TODO: system to look through history and list top x most visited pages (x set by user, default 8)
+        self.ShortcutButtons = []
+        self.PopularPages = []
+        self.columnFrames = []
+        self.newestImages = []
+        self.newestImageData = []
+        self.ShowAmount = 8
+        # Internal var
+        newestObjectText = ""
+
+        ## UI elements 
         self.addressObject = tkinter.Frame(tab)
         self.addressObject.pack(side = "top",fill = "x")
         
         self.currentAddress = tkinter.StringVar(tab,value = startpage)
 
-        self.addressBar = ttk.Combobox(self.addressObject,textvariable = self.currentAddress)
-        self.addressBar.bind("<Return>")
-        self.addressBar.pack(fill = "x")
-
-        self.historyMenu = tkinter.Menu(self.addressBar)
+        self.historyMenu = tkinter.Menu(self.addressObject)
         self.historyMenu.add_command(label="Clear history")
 
-        self.downloadMenu = tkinter.Menu(self.addressBar)
+        self.downloadMenu = tkinter.Menu(self.addressObject)
         self.downloadMenu.add_command(label="Clear downloads")
 
         self.zoom = 1
 
-        self.zoomMenu = tkinter.Menu(self.addressBar)
+        self.zoomMenu = tkinter.Menu(self.addressObject)
         self.zoomMenu.add_command(label="Zoom level: 100%",state="disabled")
         self.zoomMenu.add_command(label="+25%")
         self.zoomMenu.add_command(label="Reset")
         self.zoomMenu.add_command(label="-25%")
 
-        self.bookmarksMenu = tkinter.Menu(self.addressBar)
+        self.bookmarksMenu = tkinter.Menu(self.addressObject)
 
-        self.hamburgerMenu = tkinter.Menu(self.addressBar)
+        self.hamburgerMenu = tkinter.Menu(self.addressObject)
         self.hamburgerMenu.add_command(label="New tab")
         self.hamburgerMenu.add_command(label="New window")
         self.hamburgerMenu.add_separator()
@@ -40,19 +49,16 @@ class newTab():
         self.hamburgerMenu.add_command(label="Find")
         self.hamburgerMenu.add_cascade(label="Zoom",menu=self.zoomMenu,state="disabled")
 
-        self.menuButton = ttk.Menubutton(self.addressBar,text = "≡",menu = self.hamburgerMenu)
+        self.menuButton = ttk.Menubutton(self.addressObject,text = "≡",menu = self.hamburgerMenu)
         self.menuButton.pack(side = "right")
 
-        self.downloadButton = ttk.Menubutton(self.addressBar,text = "↓",menu = self.downloadMenu)
+        self.downloadButton = ttk.Menubutton(self.addressObject,text = "↓",menu = self.downloadMenu)
         self.downloadButton.pack(side = "right")
 
-        self.zoomButton = ttk.Menubutton(self.addressBar,text = "🔍",menu = self.zoomMenu)
+        self.zoomButton = ttk.Menubutton(self.addressObject,text = "🔍",menu = self.zoomMenu)
 
-        self.homeButton = ttk.Button(self.addressBar,text = "⌂️")
+        self.homeButton = ttk.Button(self.addressObject,text = "⌂️")
         self.homeButton.pack(side = "right")
-
-        self.gobutton = ttk.Button(self.addressBar,style="Accent.TButton",text = "Go")
-        self.gobutton.pack(side="right")
 
         # New tab design
 
@@ -64,7 +70,34 @@ class newTab():
         self.midBar = ttk.Frame(tab)
         self.midBar.pack()
 
-        self.newTabAddressBar = ttk.Combobox(self.midBar,textvariable = self.currentAddress)
+        self.newTabAddressBar = ttk.Entry(self.midBar,textvariable = self.currentAddress)
         self.newTabAddressBar.bind("<Return>")
-        self.newTabAddressBar.pack()
+        self.newTabAddressBar.pack(fill = "x",side = "left")
+        self.newTabGobutton = ttk.Button(self.midBar,style="Accent.TButton",text = "Go")
+        self.newTabGobutton.pack(side="right")
+        self.splashText = ttk.Label(tab,text="Most viewed pages",font=("TkDefaultFont",16))
+        self.splashText.pack()
+        self.PopularFrame = ttk.Frame(tab)
+        self.PopularFrame.pack(fill="both")
+        for r in range((int((self.ShowAmount)/4))):
+            self.columnFrames.append(ttk.Frame(self.PopularFrame))
+            self.columnFrames[r].pack(side = "top")
+            for c in range(4):
+                i = c+(4*r)
+                try:
+                    newestObjectText = self.PopularPages[i]
+                    self.newestImageData.append(fileHandler.noIcon)
+                    self.newestImages.append(ImageTk.PhotoImage(image=self.newestImageData[i]))
+                    print("Preset success")
+                except:
+                    newestObjectText = "Unset"
+                    self.newestImageData.append(fileHandler.noShortcut)
+                    self.newestImages.append(ImageTk.PhotoImage(image=self.newestImageData[i]))
+                    print("Preset not found")
+                else: 
+                    print("General failure")
+                
+                self.ShortcutButtons.append(ttk.Button(self.columnFrames[r],text=newestObjectText,image=self.newestImages[i],compound="top").pack(side = "left",expand=1))
+
+        self.quickAccess = ttk.Button()
 
